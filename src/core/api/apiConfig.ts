e238aa31b0fd0866bddb6584ebd5c9447f198d32
Omer Oguzoglu <omer1991mustafa@gmail.com>
2026-04-26 14:21:11 +0300

    change api-gateway

--- src/core/api/apiConfig.ts
+++ src/core/api/apiConfig.ts
@@ -4,13 +4,13 @@ export const API_CONFIG = {
   
   // Production
   // The API Gateway unifies all services under one domain.
-  PRODUCTION_GATEWAY_URL: 'https://easysy-gateway-cf5oqvjq.ew.gateway.dev',
+  PRODUCTION_GATEWAY_URL: 'https://easysy-gateway-966zw79x.ew.gateway.dev',
   
   // Development
   // Using 10.0.2.2 so Android Emulator can reach host machine's localhost
   DEV_SERVICES: {
-    hotel: 'https://easysy-gateway-cf5oqvjq.ew.gateway.dev',
-    bus: 'https://easysy-gateway-cf5oqvjq.ew.gateway.dev',
-    tour: 'https://easysy-gateway-cf5oqvjq.ew.gateway.dev',
+    hotel: 'https://easysy-gateway-966zw79x.ew.gateway.dev',
+    bus: 'https://easysy-gateway-966zw79x.ew.gateway.dev',
+    tour: 'https://easysy-gateway-966zw79x.ew.gateway.dev',
   }
 };
